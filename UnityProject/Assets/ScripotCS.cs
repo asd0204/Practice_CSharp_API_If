675eb43208935d1130275ffefb3e2cb1b8898a2c
chiu-han <55218578+chiu-han@users.noreply.github.com>
2019-10-17 12:10:58 +0800

    雞01

---
+++ UnityProject/Assets/ScripotCS.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ScripotCS : MonoBehaviour
+{
+    Animator m_Animator;
+    public Animator chicken;
+    bool 跑步開關;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            chicken.SetTrigger("吃飯觸發器");
+        }
+
+        chicken.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+    }
+}
