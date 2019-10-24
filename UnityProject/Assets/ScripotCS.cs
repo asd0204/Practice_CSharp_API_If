aa220b0e6528c3ee078fa723ae0d2af5714623ca
asd0204 <55218578+asd0204@users.noreply.github.com>
2019-10-24 10:03:01 +0800

    1024

--- UnityProject/Assets/ScripotCS.cs
+++ UnityProject/Assets/ScripotCS.cs
@@ -4,31 +4,21 @@ public class ScripotCS : MonoBehaviour
 {
     private Animator m_Animator;
 
-    private bool m_mogo;
+  
 
     void Start()
     {
         //Get the Animator attached to the GameObject you are intending to animate.
-        m_Animator = gameObject.GetComponent<Animator>();
-        m_mogo = false;
+        m_Animator = GetComponent<Animator>();
+        
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
-        {
+        if (Input.GetKey(KeyCode.E))      
             m_Animator.SetTrigger("吃飯觸發器");
-        }
-
-        if ((Input.GetKey(KeyCode.R)))
-
-        {
-            m_Animator.SetBool("跑步開關", true);
-        }
-        else
-        {
-            m_Animator.SetBool("跑步開關", false);
-        }
+            m_Animator.SetBool("跑步開關", Input.GetKey(KeyCode.R));
+       
 
     }
 
